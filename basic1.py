b37d3ddcbd63bb22b30d23706eda2a4d174c6ffc
peerapattop <peerapatza401@gmail.com>
2023-06-07 01:26:57 +0700

    อ่านภาพและแสดงผลภาพ

---
+++ basic1.py
@@ -0,0 +1,7 @@
+ #การอ่านภาพ (Read Image)
+
+import cv2 
+
+img = cv2.imread("image/cat.jpg")
+print(type(img.ndim))
+print(img)
---
+++ basic2.py
@@ -0,0 +1,9 @@
+ #การแสดงผลภาพ (Display Image)
+
+import cv2 
+
+img = cv2.imread("image/cat.jpg")
+
+cv2.imshow("Output",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
