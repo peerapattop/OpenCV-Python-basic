b5bf183289bde140f017c6343860d52971de2962
peerapattop <peerapatza401@gmail.com>
2023-06-08 20:23:07 +0700

    เปิดวิดิโอด้วย OpenCV

--- basic7.py
+++ basic7.py
@@ -1,17 +1,18 @@
 #เปิดวิดิโอด้วย OpenCV
 
-#เปิดกล้องด้วย OpenCV
-
 import cv2
 
-cap = cv2.VideoCapture("image/Video.mp4") #  0 คือ ระบุหมายเลขกล้อง 
+cap = cv2.VideoCapture("image/Video.mp4") #  ตำแหน่งวิดิโอ
 
-while (True):
+while (cap.isOpened()): #เช็คว่าวิดิโอว่าทำงานได้หรือไม่
+
     check , frame = cap.read() 
-
-    cv2.imshow("Output",frame)
-
-    if cv2.waitKey(1) & 0xFF == ord("e") : #รอรับ key ถ้ากด e จะหลุดออกจาก loop while
+    
+    if check == True: #เช็คว่าค่าที่รับมายังมีอยู่หรือไม่ ถ้าใช้ให้ทำงาน
+        cv2.imshow("Output",frame)
+        if cv2.waitKey(1) & 0xFF == ord("e"): #รอรับ key ถ้ากด e จะหลุดออกจาก loop while
+            break
+    else :
         break
 
 cap.release()
